837c39835b1c4191137d8f3f1deadeccaa4adb40
祐宏 Tiger <atigerhh@gmail.com>
2022-12-13 13:27:24 +0800

    Update get_klines.py

--- get_klines.py
+++ get_klines.py
@@ -4,29 +4,29 @@ import urllib.request
 import time
 import os
 
-# spot klines
+# u-futures klines
 
-names = ["BTC", "ETH"]
+tickers = ["BTCUSDT", "BTCUSDT_210326", "BTCUSDT_210625", "BTCUSDT_210924", "BTCUSDT_211231", "BTCUSDT_220325", "BTCUSDT_220624", "BTCUSDT_220930", "BTCUSDT_221230", 
+           "ETHUSDT", "ETHUSDT_210326", "ETHUSDT_210625", "ETHUSDT_210924", "ETHUSDT_211231", "ETHUSDT_220325", "ETHUSDT_220624", "ETHUSDT_220930", "ETHUSDT_221230"]
 
-start_date = "2021-3-1"
+start_date = "2020-12-30"
 end_date = "2022-12-04"
 
 date_range = pd.date_range(start_date, end_date, freq="D")
 parent_dir = "raw_klines/"
 
-for name in names:
-    print(name)
-    ticker = name + "USDT"
-    path = os.path.join(parent_dir, ticker+'_spot')
+for ticker in tickers:
+    print(ticker)
+    path = os.path.join(parent_dir, ticker+'_ufutures')
 
     if not os.path.exists(path):
         os.mkdir(path)
 
     for date in date_range:
-        base_url = f"https://data.binance.vision/data/spot/daily/klines/{ticker}/1m/"
+        base_url = f"https://data.binance.vision/data/futures/um/daily/klines/{ticker}/1m/"
         url = base_url + ticker + "-1m-" + date.date().isoformat() + ".zip"
         print(url)
-        file_path = os.path.join(path, ticker + '-spot-' + date.date().isoformat() + ".zip")
+        file_path = os.path.join(path, ticker + '-ufutures-' + date.date().isoformat() + ".zip")
         file_path = file_path.replace("-", "_")
 
         if not os.path.exists(file_path):
@@ -37,5 +37,3 @@ for name in names:
                 continue
 
         time.sleep(0.3)
-
-
