b6a82d6dbbb0052c406fc8a7561377d0e9cdd28c
祐宏 Tiger <atigerhh@gmail.com>
2022-12-25 15:03:42 +0800

    Create pairTrade_example.py

---
+++ Strategy/pairTrade_example.py
@@ -0,0 +1,57 @@
+# %%
+import os
+import time
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+
+import talib
+from talib import abstract
+
+# file path should under level first
+os.chdir("..")
+print(os.getcwd())
+
+from module.data import get_tidyData, DataPair
+from module.backtest import backtestingPair, dictType_output
+from module.visualize import Performance, PercentageReturnPlot
+
+# %%
+
+df_btc_origin = get_tidyData(file_name='binance_um_1m_btcusdt')
+df_eth_origin = get_tidyData(file_name='binance_um_1m_ethusdt')
+
+# %%
+
+fund = 100
+data = DataPair(df_symbolA=df_btc_origin, df_symbolB=df_eth_origin, rule='1H')
+
+# %%
+
+len1 = 12
+len2 = 2*len1
+
+data.df_pair['rsi'] = talib.RSI(data.df_pair['Close'], len1)
+data.df_pair['rsi_r1'] = data.df_pair['rsi'].rolling(len1).mean()
+data.df_pair['rsi_r2'] = data.df_pair['rsi'].rolling(len2).mean()
+
+## entry condition
+entryLong = (data.df_pair['rsi_r1'] > data.df_pair['rsi_r2']) & (data.df_pair['rsi_r1'] > 70)
+entrySellShort = (data.df_pair['rsi_r1'] < data.df_pair['rsi_r2']) & (data.df_pair['rsi_r1'] < 30)
+
+## exit condition
+exitShort = data.df_pair['rsi_r1'] < 30
+exitBuyToCover = data.df_pair['rsi_r1'] > 70
+
+data.type_setting(entryLong, entrySellShort, exitShort, exitBuyToCover)
+output_dict = dictType_output(backtestingPair(data.input_arr, exit_profitOut=True, exParam2=0.03, fund=fund))
+
+# plot & result
+result = Performance(data.df_pair['Open'], output_dict, data.idx, fund=fund, Name='Pair Trading')
+result.show_performance()
+result.draw_realized_profit()
+result.draw_equity_curve(text_position='2022-04-10')
+
+
+# %%
