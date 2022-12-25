99960602151575fa7e3972c38f2fae72f996e28b
祐宏 Tiger <atigerhh@gmail.com>
2022-12-25 14:52:52 +0800

    add pair trading function

--- module/backtest.py
+++ module/backtest.py
@@ -1,10 +1,9 @@
 import numpy as np
-import numba
 from numba import jit
 
 
 @jit(nopython=True)
-def backtesting(input_arr, fund=100, leverage=0, takerFee=0.0004, slippage=0.0008,
+def backtesting(input_arr, fund=100, leverage=0, takerFee=0.0004, slippage=0.0001,
                 exit_timeOut=False, exParam1=20, from_exit_condition=False,
                 exit_profitOut=False, exParam2=0.02, end_trigger2='C',
                 exit_lossOut=False, lossOut_condition=1, exParam3=0.05, start_trigger3='C', 
@@ -499,6 +498,403 @@ def backtesting(input_arr, fund=100, leverage=0, takerFee=0.0004, slippage=0.000
     return buy, sell, sellshort, buytocover, profit_list, profit_fee_list, profit_list_realized, profit_fee_list_realized, profit_fee_list_realized_time
 
 
+@jit(nopython=True)
+def backtestingPair(input_arr, fund=100, leverage=0, takerFee=0.0004, slippage=0.0001,
+                    exit_timeOut=False, exParam1=20, from_exit_condition=False,
+                    exit_profitOut=False, exParam2=0.02,
+                    exit_lossOut=False, lossOut_condition=1, exParam3=0.05, risk_control=False, rc_percent=0.5,
+                    exit_condition=1, exParam0=0
+                    ):
+
+    '''
+    - vol_arr, you should input ATR or standard deviation (not volume!!)
+    - leverage = 0, order with equal amount $fund
+    - when exit_condition=1 & exParam0=0, there's no exit condition
+    '''
+
+    spreadOpen_arr = input_arr[0]
+    spread_arr = input_arr[1]
+    openA_arr = input_arr[2]
+    openB_arr = input_arr[3]
+    closeA_arr = input_arr[4]
+    closeB_arr = input_arr[5]
+    entryLong_arr = input_arr[6]
+    entrySellShort_arr = input_arr[7]
+    exitShort_arr = input_arr[8]
+    exitBuyToCover_arr = input_arr[9]
+    vol_arr = input_arr[10]
+
+    LS = ''
+    buy = []
+    sell = []
+    sellshort = []
+    buytocover = []
+    profit_list = [0]
+    profit_fee_list = [0]
+    profit_list_realized = [0]
+    profit_fee_list_realized = [0]
+    profit_fee_list_realized_time = [0]
+
+    feeRate = takerFee + slippage
+    fund_money = fund
+
+    endPointHigh = spread_arr[0]
+    endPointHigh_A = closeA_arr[0]
+    endPointHigh_B = closeB_arr[0]
+    endPointLow = spread_arr[0]
+    endPointLow_A = closeA_arr[0]
+    endPointLow_B = closeB_arr[0]
+    exitSwitch = False
+    stopLoss = False
+    stopProfit = False
+    stopTime = False
+    riskControlSwitch = False
+    ts = 1
+    ts2 = 1
+    t = 0
+    exT = 0
+
+    for i in range(len(openA_arr)):
+
+        if i == len(openA_arr)-1:
+            break
+
+        if exit_lossOut == True and lossOut_condition == 4:
+            if i < exParam3:
+                continue
+
+        # stop time condition
+        if exit_timeOut == True:
+            if from_exit_condition == False:
+                if ts >= exParam1:
+                    stopTime = True
+                else:
+                    stopTime = False
+            else:
+                if exitSwitch == True:
+                    if ts2 > exParam1:
+                        stopTime = True
+                else:
+                    stopTime = False
+
+        # stop profit condition
+        if exit_profitOut == True:
+            if exitSwitch == True:
+                if LS == 'L':
+                    stopProfit = (((closeA_arr[i] / openA_arr[exT] - 1) + (1 - closeB_arr[i] / openB_arr[exT])) / 2) >= exParam2
+                elif LS == 'S':
+                    stopProfit = (((1 - closeA_arr[i] / openA_arr[exT]) + (closeB_arr[i] / openB_arr[exT] - 1)) / 2) >= exParam2
+                else:
+                    stopProfit = False
+
+        # stop loss condition
+        if exit_lossOut == True:
+            if exitSwitch == True:
+
+                if lossOut_condition == 1:
+                    if LS == 'L':
+                        stopLoss = (((closeA_arr[i] / openA_arr[exT] - 1) + (1 - closeB_arr[i] / openB_arr[exT])) / 2) <= -exParam3
+                    elif LS == 'S':
+                        stopLoss = (((1 - closeA_arr[i] / openA_arr[exT]) + (closeB_arr[i] / openB_arr[exT] - 1)) / 2) <= -exParam3
+                    else:
+                        stopLoss = False
+
+                elif lossOut_condition == 2:
+                    if LS == 'L':
+                        stopLoss = (((closeA_arr[i] / endPointHigh_A - 1) + (1 - closeB_arr[i] / endPointHigh_B)) / 2) <= -exParam3    
+                    elif LS == 'S':
+                        stopLoss = (((1 - closeA_arr[i] / endPointLow_A) + (closeB_arr[i] / endPointLow_B - 1)) / 2) <= -exParam3
+                    else:
+                        stopLoss = False
+
+                elif lossOut_condition == 3:
+                    if LS == 'L':
+                        stopLoss = spread_arr[i] <= endPointHigh - exParam3*vol_arr[exT-1]
+                    elif LS == 'S':
+                        stopLoss = spread_arr[i] >= endPointLow + exParam3*vol_arr[exT-1]
+                    else:
+                        stopLoss = False
+
+                elif lossOut_condition == 4:
+                    if LS == 'L':
+                        stopLoss = spread_arr[i] <= min(spread_arr[i-exParam3:i])
+                    elif LS == 'S':
+                        stopLoss = spread_arr[i] >= max(spread_arr[i-exParam3:i])
+                    else:
+                        stopLoss = False
+
+                ## when touch stopLoss, control risk after next trade
+                if LS != '' and risk_control == True and stopLoss == True:
+                    riskControlSwitch = True
+
+        # start exit condition
+        if LS != '' and exitSwitch == False:
+
+            if exit_condition == 1:
+                if ts >= exParam0:
+                    exitSwitch = True
+
+            elif exit_condition == 2:
+                if LS == 'L':
+                    if (((closeA_arr[i] / openA_arr[t] - 1) + (1 - closeB_arr[i] / openB_arr[t])) / 2) >= exParam0:
+                        exitSwitch = True
+                elif LS == 'S':
+                    if (((1 - closeA_arr[i] / openA_arr[t]) + (closeB_arr[i] / openB_arr[t] - 1)) / 2) >= exParam0:
+                        exitSwitch = True
+        
+            elif exit_condition == 3:
+                if LS == 'L':
+                    if spread_arr[i] >= spreadOpen_arr[t] + exParam0*vol_arr[t-1]:
+                        exitSwitch = True
+                elif LS == 'S':
+                    if spread_arr[i] <= spreadOpen_arr[t] - exParam0*vol_arr[t-1]:
+                        exitSwitch = True
+            
+            elif exit_condition == 4:
+                if LS == 'L':                    
+                    if (((closeA_arr[i] / openA_arr[t] - 1) + (1 - closeB_arr[i] / openB_arr[t])) / 2) <= -exParam0:
+                        exitSwitch = True
+                elif LS == 'S':
+                    if (((1 - closeA_arr[i] / openA_arr[t]) + (closeB_arr[i] / openB_arr[t] - 1)) / 2) <= -exParam0:
+                        exitSwitch = True
+
+            elif exit_condition == 5:
+                if LS == 'L':
+                    if spread_arr[i] <= spreadOpen_arr[t] - exParam0*vol_arr[t-1]:
+                        exitSwitch = True
+                elif LS == 'S':
+                    if spread_arr[i] >= spreadOpen_arr[t] + exParam0*vol_arr[t-1]:
+                        exitSwitch = True
+
+            elif exit_condition == 6:
+                if vol_arr[i] >= exParam0*vol_arr[t-1]:
+                    exitSwitch = True
+
+            if exitSwitch == True:
+                exT = i+1
+                endPointHigh = spread_arr[i]
+                endPointHigh_A = closeA_arr[i]
+                endPointHigh_B = closeB_arr[i]
+                endPointLow = spread_arr[i]
+                endPointLow_A = closeA_arr[i]
+                endPointLow_B = closeB_arr[i]
+
+        # no position
+        if LS == '':
+            profit_list.append(0)
+            profit_fee_list.append(0)
+            
+            if entryLong_arr[i] == True:
+                LS = 'L'
+                t = i+1
+                buy.append(t)
+
+            elif entrySellShort_arr[i] == True:
+                LS = 'S'
+                t = i+1
+                sellshort.append(t)
+
+            if entryLong_arr[i] == True or entrySellShort_arr[i] == True:
+                profit_list_realized.append(0)
+                profit_fee_list_realized.append(0)
+                profit_fee_list_realized_time.append(t)
+
+                if riskControlSwitch == False:
+                    if leverage == 0:
+                        orderMoney = fund_money
+                    else:
+                        orderMoney = fund * leverage
+                elif riskControlSwitch == True:
+                    if leverage == 0:
+                        orderMoney = fund_money * rc_percent
+                    else:
+                        orderMoney = fund * leverage * rc_percent
+
+                orderSizeA = (orderMoney / 2) / openA_arr[i+1]
+                orderSizeB = (orderMoney / 2) / openB_arr[i+1]
+
+                ts = 1
+                ts2 = 1
+                exitSwitch = False
+
+                if exit_condition == 1 and exParam0 == 0:
+                    exitSwitch = True
+                    exT = i+1
+                    endPointHigh = spread_arr[i]
+                    endPointHigh_A = closeA_arr[i]
+                    endPointHigh_B = closeB_arr[i]
+                    endPointLow = spread_arr[i]
+                    endPointLow_A = closeA_arr[i]
+                    endPointLow_B = closeB_arr[i]
+                    ts2 += 1
+
+        # long position
+        elif LS == 'L':
+            profit = orderSizeA * (openA_arr[i+1] - openA_arr[i]) + orderSizeB * (openB_arr[i] - openB_arr[i+1])
+            profit_list.append(profit)
+            ts += 1
+
+            if exitSwitch == True:
+                ts2 += 1
+                if spread_arr[i] > endPointHigh:
+                    endPointHigh = spread_arr[i]
+                    endPointHigh_A = closeA_arr[i]
+                    endPointHigh_B = closeB_arr[i]
+                if spread_arr[i] < endPointLow:
+                    endPointLow = spread_arr[i]
+                    endPointLow_A = closeA_arr[i]
+                    endPointLow_B = closeB_arr[i]
+
+            if exitShort_arr[i] == True or i == len(openA_arr)-2 or stopLoss or stopProfit or stopTime: 
+                pl_round = orderSizeA * (openA_arr[i+1] - openA_arr[t]) + orderSizeB * (openB_arr[t] - openB_arr[i+1])
+                profit_fee = profit - orderMoney*feeRate - (orderMoney+pl_round)*feeRate
+                profit_fee_list.append(profit_fee)
+                sell.append(i+1)
+                
+                # Realized PnL
+                profit_realized = pl_round
+                profit_fee_realized = pl_round - orderMoney*feeRate - (orderMoney+pl_round)*feeRate
+                fund += profit_fee_realized
+
+                profit_list_realized.append(profit_realized)
+                profit_fee_list_realized.append(profit_fee_realized)
+                profit_fee_list_realized_time.append(i+1)
+
+                # close riskControlSwitch
+                if riskControlSwitch == True and profit_fee_realized > 0:
+                    riskControlSwitch = False
+
+                # reset stop condition
+                exitSwitch = False
+                stopLoss = False
+                stopProfit = False
+                stopTime = False
+                ts = 1
+                ts2 = 1
+
+                if entrySellShort_arr[i] == False:
+                    LS = ''
+                
+                ## turn over
+                elif entrySellShort_arr[i] == True:
+                    LS = 'S'
+                    t = i+1
+                    sellshort.append(t)
+
+                    if riskControlSwitch == False:
+                        if leverage == 0:
+                            orderMoney = fund_money
+                        else:
+                            orderMoney = fund * leverage
+                    elif riskControlSwitch == True:
+                        if leverage == 0:
+                            orderMoney = fund_money * rc_percent
+                        else:
+                            orderMoney = fund * leverage * rc_percent
+
+                    orderSizeA = (orderMoney / 2) / openA_arr[i+1]
+                    orderSizeB = (orderMoney / 2) / openB_arr[i+1]
+
+                    if exit_condition == 1 and exParam0 == 0:
+                        exitSwitch = True
+                        exT = i+1
+                        endPointHigh = spread_arr[i]
+                        endPointHigh_A = closeA_arr[i]
+                        endPointHigh_B = closeB_arr[i]
+                        endPointLow = spread_arr[i]
+                        endPointLow_A = closeA_arr[i]
+                        endPointLow_B = closeB_arr[i]
+                        ts2 += 1
+
+            else:
+                profit_fee = profit
+                profit_fee_list.append(profit_fee)
+
+        # short position
+        elif LS == 'S':
+            profit = orderSizeA * (openA_arr[i] - openA_arr[i+1]) + orderSizeB * (openB_arr[i+1] - openB_arr[i])
+            profit_list.append(profit)
+            ts += 1
+
+            if exitSwitch == True:
+                ts2 += 1
+                if spread_arr[i] > endPointHigh:
+                    endPointHigh = spread_arr[i]
+                    endPointHigh_A = closeA_arr[i]
+                    endPointHigh_B = closeB_arr[i]
+                if spread_arr[i] < endPointLow:
+                    endPointLow = spread_arr[i]
+                    endPointLow_A = closeA_arr[i]
+                    endPointLow_B = closeB_arr[i]
+
+            if exitBuyToCover_arr[i] == True or i == len(openA_arr)-2 or stopLoss or stopProfit or stopTime: 
+                pl_round = orderSizeA * (openA_arr[t] - openA_arr[i+1]) + orderSizeB * (openB_arr[i+1] - openB_arr[t])
+                profit_fee = profit - orderMoney*feeRate - (orderMoney+pl_round)*feeRate
+                profit_fee_list.append(profit_fee)
+                buytocover.append(i+1)
+                
+                # Realized PnL
+                profit_realized = pl_round
+                profit_fee_realized = pl_round - orderMoney*feeRate - (orderMoney+pl_round)*feeRate
+                fund += profit_fee_realized
+
+                profit_list_realized.append(profit_realized)
+                profit_fee_list_realized.append(profit_fee_realized)
+                profit_fee_list_realized_time.append(i+1)
+
+                # close riskControlSwitch
+                if riskControlSwitch == True and profit_fee_realized > 0:
+                    riskControlSwitch = False
+
+                # reset stop condition
+                exitSwitch = False
+                stopLoss = False
+                stopProfit = False
+                stopTime = False
+                ts = 1
+                ts2 = 1
+
+                if entryLong_arr[i] == False:
+                    LS = ''
+
+                ## turn over
+                elif entryLong_arr[i] == True:
+                    LS = 'L'
+                    t = i+1
+                    buy.append(t)
+
+                    if riskControlSwitch == False:
+                        if leverage == 0:
+                            orderMoney = fund_money
+                        else:
+                            orderMoney = fund * leverage
+                    elif riskControlSwitch == True:
+                        if leverage == 0:
+                            orderMoney = fund_money * rc_percent
+                        else:
+                            orderMoney = fund * leverage * rc_percent
+
+                    orderSizeA = (orderMoney / 2) / openA_arr[i+1]
+                    orderSizeB = (orderMoney / 2) / openB_arr[i+1]
+
+                    if exit_condition == 1 and exParam0 == 0:
+                        exitSwitch = True
+                        exT = i+1
+                        endPointHigh = spread_arr[i]
+                        endPointHigh_A = closeA_arr[i]
+                        endPointHigh_B = closeB_arr[i]
+                        endPointLow = spread_arr[i]
+                        endPointLow_A = closeA_arr[i]
+                        endPointLow_B = closeB_arr[i]
+                        ts2 += 1
+
+            else:
+                profit_fee = profit
+                profit_fee_list.append(profit_fee)
+
+    return buy, sell, sellshort, buytocover, profit_list, profit_fee_list, profit_list_realized, profit_fee_list_realized, profit_fee_list_realized_time
+
+
 def dictType_output(output_tuple):
     
     output_dict = {'buy': output_tuple[0], 'sell': output_tuple[1], 'sellshort': output_tuple[2], 'buytocover': output_tuple[3],
@@ -506,4 +902,6 @@ def dictType_output(output_tuple):
                    'profit_fee_list_realized': output_tuple[7], 'profit_fee_list_realized_time': output_tuple[8]}
     
     return output_dict
+
+
 
