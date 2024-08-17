1a35098e48341ef181c25fefe7584e3fd7b762a8
YU HUNG, CHIANG <atigerhh@gmail.com>
2024-08-17 15:08:06 +0800

    Update parametersOptimize_example.py

--- Strategy/parametersOptimize_example.py
+++ Strategy/parametersOptimize_example.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 os.chdir("..")
 print(os.getcwd())
 
-from module.optimization import GeneticAlgorithm, RandomSearch, GridSearch, SimulatedAnnealing
+from module.optimization import GeneticAlgorithm, RandomSearch, GridSearch, SimulatedAnnealing, ParticleSwarmOptimization
 
 # %%
 
@@ -30,7 +30,6 @@ def fitness_func(individual):
 
     return fitness_value
 
-
 # %%
 
 ### Genetic Algorithm ###
@@ -47,13 +46,12 @@ SELECTION_STG = 'uniform'
 MOVING_STG = True
 expended_ratio = 0.15
 MOVE_RATE = 0.25
-save_process = True
+save_process = False
 
 ga_obj = GeneticAlgorithm(domain_range, fitness_func, INI_SIZE, POP_SIZE, ELITE_SIZE, 
                           GENERATIONS, CROSSOVER_RATE, MUTATION_RATE, OBJECTIVE, 
                           SELECTION_STG, MOVING_STG, expended_ratio, MOVE_RATE,
                           save_process)
-
 
 result = ga_obj.optimize()
 result
@@ -65,7 +63,7 @@ result
 # Hyperparameters
 OBJECTIVE = 'maximum'
 search_times = 1500
-save_process = True
+save_process = False
 
 rs_obj = RandomSearch(domain_range, fitness_func, OBJECTIVE, search_times, save_process)
 
@@ -84,12 +82,44 @@ Q = 5
 expended_ratio = 0.15 
 max_count = 1500
 OBJECTIVE = 'maximum'
-save_process = True
+save_process = False
 
 sa_obj = SimulatedAnnealing(domain_range, fitness_func, T0, Tf, alpha, Q, 
                             expended_ratio, max_count, OBJECTIVE, save_process)
 
 result = sa_obj.optimize()
+result
+
+# %%
+
+### Particle Swarm Optimization ###
+
+# Hyperparameters
+OBJECTIVE = 'maximum'
+num_particles = 50
+num_iteration = 30
+Velocity_Strategy = 'zero'
+W_MAX = 0.8
+W_MIN = 0.2
+W_Strategy = 'decrease'
+C1_MAX = 2
+C1_MIN = 0.9
+C1_Strategy = 'increase'
+C2_MAX = 2
+C2_MIN = 0.9
+C2_Strategy = 'decrease'
+R_MAX = 0
+R_MIN = 0
+R_Strategy = 'increase'
+variable_type = 'discrete'
+save_process = False
+
+pso_obj = ParticleSwarmOptimization(domain_range, fitness_func, OBJECTIVE, num_particles, 
+                                    num_iteration, Velocity_Strategy, W_MAX, W_MIN, W_Strategy, 
+                                    C1_MAX, C1_MIN, C1_Strategy, C2_MAX, C2_MIN, C2_Strategy, 
+                                    R_MAX, R_MIN, R_Strategy, variable_type, save_process)
+
+result = pso_obj.optimize()
 result
 
 # %%
@@ -106,7 +136,7 @@ domain_range = [
 
 # Hyperparameters
 OBJECTIVE = 'maximum'
-save_process = True
+save_process = False
 
 gs_obj = GridSearch(domain_range, fitness_func, OBJECTIVE, save_process)
 
@@ -114,4 +144,3 @@ result = gs_obj.optimize()
 result
 
 # %%
-
